7c19c1c114abc90083fa38deb64e33d95fa538c3
Max Jöhnk <maxjoehnk@gmail.com>
2020-01-02 20:23:26 +0100

    feat(playlists): always assume playlist id

--- src/http/all_playlists.rs
+++ src/http/all_playlists.rs
@@ -11,7 +11,7 @@ pub type GetAllPlaylistsResponse = GMusicListResponse<Playlist>;
 #[derive(Debug, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Playlist {
-    pub id: Option<String>,
+    pub id: String,
     pub name: String,
     pub deleted: Option<bool>,
     #[serde(rename = "type")]
