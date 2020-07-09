b4121d0d0854e44d9449acf0d0e47cf4186f7f51
Max Jöhnk <maxjoehnk@gmail.com>
2020-07-09 07:25:23 +0200

    fix(track): expose rating enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub use crate::api::GoogleMusicApi;
 pub use crate::models::album::Album;
 pub use crate::models::all_playlists::{Playlist, PlaylistShareState, PlaylistType};
-pub use crate::models::all_tracks::Track;
+pub use crate::models::all_tracks::{Track, TrackRating};
 pub use crate::models::artist::Artist;
 pub use crate::models::playlist_entries::PlaylistEntry;
 pub use crate::models::search_results::SearchResult;
